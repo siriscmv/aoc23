d6a4f474c3f1b4938b4865f1dcaa71e57024bf84
Siris <40269790+Siris01@users.noreply.github.com>
2023-12-04 18:00:43 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Advent of Code 2023
+
+- List of problems can be found [here](https://adventofcode.com/2023/)
+- Most of the solutions have a naive approach, and are not optimized for speed
+
+## Running solutions
+
+- `deno run -A main.ts <day> <part>`
---
+++ main.ts
@@ -0,0 +1,19 @@
+const pad = (n: string) => {
+	if (parseInt(n) < 9) return `0${n}`;
+	return n;
+};
+
+const args = Deno.args;
+
+const day = pad(args[0]);
+const part = parseInt(args[1]);
+
+const data = await Deno.readFile(`./inputs/d${day}.txt`);
+const input: string[] = new TextDecoder()
+	.decode(data)
+	.split('\n')
+	.map((l) => l.trim());
+const runner = await import(`./solutions/d${day}.ts`);
+
+const output = part === 1 ? await runner.p1(input) : await runner.p2(input);
+console.log(output);
